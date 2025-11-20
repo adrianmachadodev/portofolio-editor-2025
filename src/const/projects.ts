40a91ab6dc09a409f8eb2b634c6aa0f3f547b026
Adrian Machado <machadoadriandev@gmail.com>
2025-11-20 19:35:09 -0300

    section projects and data

---
+++ src/const/projects.ts
@@ -0,0 +1,28 @@
+export const PROJECTS = [
+    {
+        id:1,
+        name: 'Presentación Portfolio 2022 (Desarrollador Web)',
+        year: 2022,
+        image: '/public/images/projects/portfolio-angular.png',
+    },
+    {
+        id:2,
+        name: 'Video Navideño para muestra',
+        year: 2024,
+        image: '/public/images/projects/portfolio-angular.png',
+    },
+    {
+        id:3,
+        name: 'Presentación Portfolio 2025 (Editor de Video)',
+        year: 2024,
+        image: '/public/images/projects/portfolio-angular.png',
+    },
+    {
+        id:4,
+        name: 'Presentación Portfolio 2022 (Desarrollador Web)',
+        year: 2022,
+        image: '/public/images/projects/portfolio-angular.png',
+    },
+
+
+]
