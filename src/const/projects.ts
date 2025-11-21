606ffa5ebdaa671965f9ccca9bd80a664332ba51
Adrian Machado <machadoadriandev@gmail.com>
2025-11-21 20:11:28 -0300

    Section reels and videos horizontals

--- src/const/projects.ts
+++ src/const/projects.ts
@@ -4,25 +4,52 @@ export const PROJECTS = [
         name: 'Presentación Portfolio 2022 (Desarrollador Web)',
         year: 2022,
         image: '/public/images/projects/portfolio-angular.png',
+        type:'Horizontal'
     },
     {
         id:2,
         name: 'Video Navideño para muestra',
         year: 2024,
-        image: '/public/images/projects/portfolio-angular.png',
+        image: '/public/images/projects/mab.jpg',
+        type: 'Reel'
     },
+    
     {
         id:3,
         name: 'Presentación Portfolio 2025 (Editor de Video)',
         year: 2024,
         image: '/public/images/projects/portfolio-angular.png',
+        type: 'Horizontal'
+
     },
     {
         id:4,
         name: 'Presentación Portfolio 2022 (Desarrollador Web)',
         year: 2022,
         image: '/public/images/projects/portfolio-angular.png',
+        type: 'Horizontal'
+
+        
     },
+    {
+        id:5,
+        name: 'Presentación Portfolio 2022 (Desarrollador Web)',
+        year: 2022,
+        image: '/public/images/projects/portfolio-angular.png',
+        type: 'Reel'
+
+        
+    },
+    {
+        id:6,
+        name: 'Presentación Portfolio 2022 (Desarrollador Web)',
+        year: 2022,
+        image: '/public/images/projects/portfolio-angular.png',
+        type: 'Reel'
+
+        
+    },
+    
 
 
 ]
