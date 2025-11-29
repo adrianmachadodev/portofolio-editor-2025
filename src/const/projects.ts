fa16ddf1826b04f7e87033b7d11d23bc67e97a62
Adrian Machado <machadoadriandev@gmail.com>
2025-11-29 14:26:31 -0300

    card, header, and details

--- src/const/projects.ts
+++ src/const/projects.ts
@@ -5,16 +5,19 @@ export const PROJECTS = [
     year: 2022,
     image: "/public/images/projects/portfolio-angular.png",
     type: "Horizontal",
+    software:'Premiere Pro',
     url: "https://www.youtube.com/embed/MI6a6TBJGUE?si=nPFzH3sgOy2iTLI_",
     description:
       "Proyecto de presentación del portfolio como desarrollador que realicé en 2022. He utilizado iconos relacionados a las distintas tecnologías que utilizo.",
   },
   {
     id: 2,
-    name: "Reel presentación de fiesta",
+    name: "Reel presentación de productos Navideños",
     year: 2025,
     image: "/public/images/projects/final-reel-mab.jpg",
     type: "Reel",
+    software:'Premiere Pro',
+    url: "https://youtube.com/embed/R5DMIMml7FI?si=VyO96PlXbG2scn8s",
     description:
       "Reel diseñado para presentar una variedad de productos navideños a través de planos cercanos, colores cálidos y un ritmo visual que acompaña el espíritu de las fiestas.",
   },
@@ -23,6 +26,7 @@ export const PROJECTS = [
     id: 3,
     name: "Presentación Portfolio 2025 (Editor de Video)",
     year: 2024,
+    software:'Premiere Pro',
     image: "/public/images/projects/portfolio-angular.png",
     type: "Horizontal",
     description:
