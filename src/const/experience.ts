3a014bd7f84a38368c42ea6935f3ec2ea4eb9068
Adrian Machado <machadoadriandev@gmail.com>
2026-08-11 10:40:55 -0300

    update portfolio

---
+++ src/const/experience.ts
@@ -0,0 +1,23 @@
+export const EXPERIENCE = [
+  {
+    title: "Content Creator (Video + IA)",
+    company: "Software Factory AI",
+    date: "Apr. 2026 - present",
+    description:
+      "In this project, my role as a content creator for a vehicle agency network powered by artificial intelligence, creating videos and images with different tools, where these are published on TikTok, Instagram, and WhatsApp",
+  },
+  {
+    title: "Social Media Video Editor",
+    company: "Freelance",
+    date: "Nov. 2025 - Present",
+    description:
+      "Created and edited short-form vertical video content for Instagram Reels and TikTok, focused on audience retention and engagement. Applied dynamic cuts, transitions, motion graphics, audiovisual synchronization, color correction, and platform-specific optimization.",
+  },
+  {
+    title: "Video editor - Youtube",
+    company: "Freelance",
+    date: "Apr. 2020 - Jan. 2023",
+    description:
+      "Edited horizontal YouTube content with dynamic cuts, transitions, and audiovisual synchronization to maintain engaging visual flow. Applied color correction and basic color grading while efficiently managing footage, assets, and project files to streamline the editing workflow.",
+  },
+];
