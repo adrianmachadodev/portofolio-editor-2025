b0eba0b9c95af7f9d0a460cac505dbf6b61e71e5
Adrian Machado <machadoadriandev@gmail.com>
2026-08-18 11:33:49 -0300

    faq section

---
+++ src/const/faq.ts
@@ -0,0 +1,27 @@
+export const FAQ = [
+  {
+    question: "How much experience working as a video editor?",
+    response:
+      "I have 3 years of experience as a video editor. I worked for a client creating content for their YouTube channel between these years",
+  },
+  {
+    question: "What is the price of the videos?",
+    response:
+      "The price is $25 USD per video for international clients, or ARS $25,000 per video in Argentina.",
+  },
+  {
+    question: "How long does it take to edit a video?",
+    response:
+      "Most videos are delivered within 2-4 business days.",
+  },
+  {
+    question: "Do you work with Generative AI?",
+    response:
+      "Yes, I do. I work with Google vids, Adobe Firefly, RunWayMl, HeyGen, ChatGPT, Gemini & more.",
+  },
+  {
+    question: "What is you main software?",
+    response:
+      "I use Adobe Premiere Pro.",
+  },
+];
